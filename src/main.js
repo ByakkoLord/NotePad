46f9d2cea130be0be2b1d20e6e4caaee556b6162
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-07-08 20:42:17 -0300

    Add Error Message

--- src/main.js
+++ src/main.js
@@ -8,15 +8,22 @@ add_button.addEventListener('click', () => {
 
 let add_button_note = document.querySelector('#add_button_note')
 let text_note = document.querySelector('#text_note')
+let box_required = document.querySelector('#box_required')
 
 add_button_note.addEventListener('click', () => {
     let note = document.createElement('div')
 
-    add_note.classList.toggle("hidden")
-    add_note.classList.toggle("flex")
+    if (text_note.value == '') {
+        box_required.classList.toggle('hidden')
+        box_required.classList.toggle('flex')
     
-    
-    note.className = 'flex justify-between items-center h-16 border-b-4 border-slate-600 ml-3 mr-3'
-    note.innerHTML = `<h3 class="ml-5 font-bold text-lg">${text_note.value}</h3><button id="add_button" class="rotate-45 flex justify-center items-center w-9 h-9 hover:bg-slate-200 hover:shadow-xl rounded-full transition-all mr-5"><i class="fa-solid fa-plus text-slate-600 text-4xl rounded-full"></i></button>`
-    document.querySelector('.notes').appendChild(note)
+    } else{
+        add_note.classList.toggle("hidden")
+        add_note.classList.toggle("flex")
+        
+        note.className = 'flex justify-between items-center custonh border-b-4 border-slate-600 ml-3 mr-3'
+        note.innerHTML = `<h3 class="ml-5 font-bold text-lg break-words">${text_note.value}</h3><button id="add_button" class="rotate-45 flex justify-center items-center w-9 h-9 hover:bg-slate-200 hover:shadow-xl rounded-full transition-all mr-5"><i class="fa-solid fa-plus text-slate-600 text-4xl rounded-full"></i></button>`
+        document.querySelector('.notes').appendChild(note)
+
+    }
 })
